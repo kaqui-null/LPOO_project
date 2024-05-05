2b395566742025c8128e647fe47bbfc25b803396
kaqui-null <emanuelgomes24.08@gmail.com>
2024-05-05 10:42:30 -0300

    Adicionada a organização de classes e packages

---
+++ Characters/Enemies/Enemies.java
@@ -0,0 +1,11 @@
+package Characters.Enemies;
+
+import Characters.NPC;
+
+public class Enemies extends NPC{
+    
+    public Enemies(int[] newPosition, int[] newHealthMana, int[] newStats){
+        super(newPosition, newHealthMana, newStats);
+    }
+
+}
---
+++ Menu/PartyMenu.java
@@ -0,0 +1,7 @@
+package Menu;
+
+// Add mapas em formato spritesheet e blacklist alguns sprites
+
+public class PartyMenu extends Menu{
+    
+}
