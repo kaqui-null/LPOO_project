504028b42472405f63d9a86b0ee0f47b358d2c83
Guilherme Cândido <guicmayrinck@gmail.com>
2024-05-09 14:20:22 -0300

    BattleMenu

--- Menu/BattleMenu.java
+++ Menu/BattleMenu.java
@@ -18,9 +18,6 @@ public class BattleMenu extends JFrame {
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 
-	/**
-	 * Launch the application.
-	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
