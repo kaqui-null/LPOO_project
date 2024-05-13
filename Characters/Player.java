6c2aed06c0053482330a82c031f0fab6f8fa453d
kaqui-null <emanuelgomes24.08@gmail.com>
2024-05-13 18:25:16 -0300

    adicionado simulação de batalha pelo terminal

--- Characters/Player.java
+++ Characters/Player.java
@@ -1,7 +1,41 @@
 package Characters;
 
 public class Player extends NPC {
-    public Player(int[] positon, int[] healthMana, int[] Stats) {
-        super(positon, healthMana, Stats);
+	
+	private int[] position;
+    public Player(int[] healthMana, int[] Stats) {
+        super(healthMana, Stats);
+    }
+    
+    public int defend(int damageRecieved) {
+    	if (damageRecieved - getDefense() < 0) {
+    		return 0;
+    	}
+    	return damageRecieved - getDefense();
+    }
+    
+    // getters setters
+    // position = {x,y}
+    public void setPosition(int[] newPosition) {
+    	position = newPosition;
+    }
+    public void setPosition(int x, int y) {
+    	position[0] = x;
+    	position[1] = y;
+    }
+    public int[] getPosition() {
+        return position;
+    }
+    public void setX(int x) {
+    	position[0] = x;
+    }
+    public void setY(int y) {
+    	position[0] = y;
+    }
+    public int getX(){
+        return position[0];
+    }
+    public int getY(){
+        return position[1];
     }
 }
