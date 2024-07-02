e5845de8ab9b4125de1fa2e4b60f3a451c4000b5
kaqui-null <emanuelgomes24.08@gmail.com>
2024-07-01 23:44:26 -0300

    revert my changes

--- Main.java
+++ Main.java
@@ -18,72 +18,10 @@ public class Main{
 	public static void main(String[] args) {
 		Player player = new Player(playerHealthMana, playerStats);
 		Enemies enemy = new Enemies(enemyHealthMana, enemyStats);
-		Mapa mapa = new Mapa();
 		BattleMenu battleMenu = new BattleMenu(enemy, null);
-		
-		while (true) {
-			System.out.println(battleMenu.getCurrChoice());
-		}
-		
-		
-		//mapa(mapa, player);
-		//battle(player, enemy);
+		Mapa mapa = new Mapa(player);
 		
 	}
-	
-	public static void mapa(Mapa mapa, Player player) { 
-		Scanner scr = new Scanner(System.in);
-
-		mapa.draw();
-		mapa.setNewMap(mapa.replacePlayerSprite(player.getPosition(), player.getTerminalSprite()));
-		while (true) {
-			char key = scr.next().charAt(0);
-			player.walk(key);
-			mapa.setNewMap(mapa.replacePlayerSprite(player.getPosition(), player.getTerminalSprite()));
-			
-			mapa.draw();
-			
-			if (key == 'x') {
-				break;
-			}
-		}
-		scr.close();
-	}
-	
-	// get new enemy through rng table of the place
-	public static void battle(Player player, Enemies enemy) {
-		Scanner scanner = new Scanner(System.in);
-		
-		while (player.isAlive()) {
-			System.out.println("Enemy: " + String.valueOf(enemy.getHealth()) + " / " + String.valueOf(enemyHealthMana[0]));
-			System.out.println("Player: " + String.valueOf(player.getHealth()) + " / " + String.valueOf(playerHealthMana[0]));
-			System.out.println("O que vc quer fazer?\n");
-			System.out.println("Atacar[1]\nDefender[2]");
-			battleMenuChoice = scanner.nextInt();
-			
-			if (player.getAgility() >= enemy.getAgility() && battleMenuChoice == 1) {
-				enemy.setHealth(enemy.getsHurt(enemy.getHealth() , player.attack(enemy.getDefense())));
-				System.out.println("The enemy attacks you");
-				player.setHealth(player.getsHurt(player.getHealth() , enemy.attack(player.getDefense())));
-			} else if (battleMenuChoice == 2) {
-				player.setHealth(player.getsHurt(player.getHealth() , player.defend(enemy.attack(player.getDefense()))));
-			} else {
-				player.setHealth(player.getsHurt(player.getHealth() , enemy.attack(player.getDefense())));
-				enemy.setHealth(enemy.getsHurt(player.getHealth() , player.attack(enemy.getDefense())));
-			}
-			
-			if (enemy.getHealth() <= 0) {
-				System.out.println("You win");
-				break;
-			} else if (player.getHealth() <= 0) {
-				System.out.println("You lose");
-				break;
-			}
-		}
-		
-		scanner.close();
-	}
-
 }
 
 
---
+++ Mapas/PlayerMov.java
@@ -0,0 +1,78 @@
+package Mapas;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+import Characters.Players.Player;
+
+public class PlayerMov extends JPanel implements ActionListener, KeyListener{
+
+    private int x, y;
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    private Player player;
+
+    public PlayerMov(Player player) {
+        Timer timer = new Timer(30, this);
+        timer.start();
+        this.player = player;
+        x = player.getX();
+        y = player.getY();
+        addKeyListener(this);
+        setFocusable(true);
+    }
+
+    public PlayerMov(Player player, int x, int y) {
+        Timer timer = new Timer(30, this);
+        timer.start();
+        this.player = player;
+        setX(x);                                    
+        setY(y);
+        player.setX(x);
+        player.setY(y);
+        addKeyListener(this);
+        setFocusable(true);
+    }
+
+    @Override
+    public void paintComponents(Graphics g) {
+        super.paintComponents(g);
+        g.setColor(Color.blue);
+        g.fillRect(x, y, 50, 50);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        repaint();
+    }
+
+    @Override
+    public void keyReleased(KeyEvent k) {
+        //not used
+    }
+
+    @Override
+    public void keyTyped(KeyEvent k) {
+        //not used
+    }
+
+    @Override
+    public void keyPressed(KeyEvent k) {
+        player.walk(k.getKeyChar());
+        setX(player.getX());
+        setY(player.getY());
+        repaint();
+    }
+}
