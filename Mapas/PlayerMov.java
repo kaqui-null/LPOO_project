86c83f1daa89deadc59b8e8a393f7d81745504f3
kaqui-null <emanuelgomes24.08@gmail.com>
2024-07-20 19:31:50 -0300

    fix merge problems

--- Mapas/PlayerMov.java
+++ Mapas/PlayerMov.java
@@ -4,65 +4,6 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-<<<<<<< HEAD
-import java.util.Scanner;
-
-import javax.swing.JPanel;
-import javax.swing.JFrame;
-
-import Characters.Players.Player;
-
-public class PlayerMov extends JPanel implements ActionListener{
-
-    private int x, y;
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    public void setY(int y) {
-        this.y = y;
-    }
-
-    private Player player;
-
-    public PlayerMov(Player player) {
-        this.player = player;
-        x = player.getX();
-        y = player.getY();
-    }
-
-    public PlayerMov(Player player, int x, int y) {
-        this.player = player;
-        setX(x);
-        setY(y);
-        player.setX(x);
-        player.setY(y);
-    }
-
-    @Override
-    public void paintComponents(Graphics g) {
-        super.paintComponents(g);
-        g.setColor(Color.blue);
-        g.fillRect(x, y, 50, 50);
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        player.walk(getKey());
-        setX(player.getX());
-        setY(player.getY());
-        repaint();
-    }
-
-    public char getKey() {
-        Scanner scr = new Scanner(System.in);
-        String returnString = scr.nextLine();
-        scr.close();
-        return returnString.charAt(0);
-    }
-}
-
-=======
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import javax.swing.JPanel;
@@ -135,4 +76,3 @@ public class PlayerMov extends JPanel implements ActionListener, KeyListener{
         repaint();
     }
 }
->>>>>>> branch 'main' of https://github.com/kaqui-null/LPOO_project.git
